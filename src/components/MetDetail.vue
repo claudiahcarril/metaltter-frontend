2ee2c5bd6e0170206a784c308e00f9f20db05431
Claudia Hernández <claudiacarril8@gmail.com>
2023-02-03 15:51:24 +0100

    Creating Login and Register Views and first components

---
+++ src/components/MetDetail.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="met">
+        <slot name="user-avatar"></slot>
+        <div>
+            <slot name="user-name"></slot>
+            <slot name="date-created"></slot>
+        </div>
+        <slot name="username"></slot>
+        <div class="user-message">
+            <slot name="message"></slot>
+            <slot name="kudos"></slot>
+        </div>
+    </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'MetDetail',
+})
+</script>
